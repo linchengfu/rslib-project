4ff05584c28a705b80e478e01cd32715208e8d4c
lcf-air <linchengfu2000@gmail.com>
2025-08-16 10:32:10 +0800

    fix: build error

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,3 @@ export {default as Calendar} from './Calendar';
 export {default as Dashboard} from './Dashboard';
 export {default as PieChart} from './PieChart';
 export {default as Union} from './Union';
-export {default as zhifubao} from './zhifubao';
