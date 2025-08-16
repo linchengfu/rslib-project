31ccd821f230954950d7836b5e44856f1f35cb5a
GitHub Action <action@github.com>
2025-08-16 03:06:31 +0000

    chore: sync icons from Figma and bump version to v1.0.2 [skip ci]

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export {default as Calendar} from './Calendar';
 export {default as Dashboard} from './Dashboard';
+export {default as Gear} from './Gear';
 export {default as PieChart} from './PieChart';
 export {default as Union} from './Union';
