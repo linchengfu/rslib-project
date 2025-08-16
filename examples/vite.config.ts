f0ae7409e1888a97128e059b51d0f43a4111acf2
lcf-air <linchengfu2000@gmail.com>
2025-08-16 15:53:19 +0800

    fix: delete vercel.json

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   root: __dirname, // 指定 examples 目录为根目录
@@ -13,7 +14,7 @@ export default defineConfig({
     open: true,
   },
   build: {
-    outDir: process.env.VERCEL ? '../dist' : 'dist', // Vercel 输出到根目录的 dist，其他输出到 examples/dist
+    outDir: process.env.VERCEL ? path.resolve(__dirname, '../dist') : 'dist', // Vercel 输出到项目根目录的 dist，其他输出到 examples/dist
     emptyOutDir: true, // 构建前清空输出目录
   },
 });
